49373fb241eb99852ce66e7d03e031b5fb60767d
yan.hemba <yan.hemba@toweya.net>
2019-08-14 02:03:29 +0300

    add file-downloading interface

--- admin_panel/exel/admin.py
+++ admin_panel/exel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AversFlat
+from .models import AversFlat, InputExel
 # Register your models here.
 
 admin.site.register(AversFlat)
+admin.site.register(InputExel)
--- admin_panel/exel/models.py
+++ admin_panel/exel/models.py
@@ -1,10 +1,20 @@
+import datetime
+import logging
+
 from django.db import models
 from .exel_performer import WorkExel
-
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return f'XML/{filename}'
+
+
+class InputExel(models.Model):
+
+    current_file = models.FileField()
+
+    def __str__(self):
+        return str(self.current_file)
 
 
 class AversFlat(models.Model):
@@ -15,3 +25,15 @@ class AversFlat(models.Model):
              update_fields=None):
         worker_exel = WorkExel(self.file)
         worker_exel.main_perform()
+
+        today = datetime.date.today()
+        file_result_path = f'results/{today}.csv'
+        try:
+            input_file_instance = InputExel()
+            logging.warning(f'Making instance')
+            input_file_instance.current_file = file_result_path
+            logging.warning(f'Specify field path')
+            input_file_instance.save()
+            logging.warning(f'Save')
+        except Exception as e:
+            logging.warning(f'Error - {e}')
